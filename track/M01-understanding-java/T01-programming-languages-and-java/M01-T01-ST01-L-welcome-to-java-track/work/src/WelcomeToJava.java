ef7266ac2d32f7e06707f5ee15a2fa1160938e17
Kavyagpp <kavyagp2108@gmail.com>
2026-07-30 23:53:31 +0530

    one line added

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,9 +1,11 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       System.out.println("welcome to java track");
-       System.out.println("my name is kavya");
-       System.out.println("my goal is to become a software engineer");
-       System.out.println("i selected java because i want to build large applications.");
+        System.out.println("Welcome to java track");
+        System.out.println("My name is kavya");
+        System.out.println("My goal is to become a software engineer");
+        System.out.println("I selected java because i want to build large applications.");
     }
 
 }
