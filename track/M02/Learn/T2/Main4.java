ce71a8318cc97be0e5beb419e3c6a6826236526c
Kavyagpp <kavyagp2108@gmail.com>
2026-08-05 15:28:38 +0530

    initial commit

--- track/M02/Learn/T2/Main4.java
+++ track/M02/Learn/T2/Main4.java
@@ -2,10 +2,10 @@
 public class Main4 {
 
     public static void main(String[] args) {
-        int i = 10;
-        int r = i++ + ++i + i++ + i++ + ++i + i++;
+        int a = 10;
+        int r = a++ + ++a + a++ + a++ + ++a + a++;
 
         System.out.println(r);
-        System.out.println(i);
+        System.out.println(a);
     }
 }
