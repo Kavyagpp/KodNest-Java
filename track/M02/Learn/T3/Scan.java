87feacd2435d37c5ecd02db26672ac3ea0a5dcfa
Kavyagpp <kavyagp2108@gmail.com>
2026-08-11 11:24:47 +0530

    initial commit

--- track/M02/Learn/T3/Scan.java
+++ track/M02/Learn/T3/Scan.java
@@ -6,31 +6,51 @@ public class Scan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter byte value:");
-        byte a = sc.nextByte();
+        byte a = sc.nextByte();// it will read only byte value
         System.out.println(a);
 
         System.out.println("Enter short Value:");
-        short b = sc.nextShort();
+        short b = sc.nextShort();// it will read only short value
         System.out.println(b);
 
         System.out.println("Enter integer value:");
-        int c = sc.nextInt();
+        int c = sc.nextInt();//it will read only int value
         System.out.println(c);
 
         System.out.println("Enter Long value:");
-        long d = sc.nextLong();
+        long d = sc.nextLong();//it will read only long value
         System.out.println(d);
 
         System.out.println("Enter Float value:");
-        float e = sc.nextFloat();
+        float e = sc.nextFloat();//it will read only float value
         System.out.println(e);
 
         System.out.println("Enter Double value:");
-        double f = sc.nextDouble();
+        double f = sc.nextDouble();//it will read only double value
         System.out.println(f);
 
         System.out.println("Enter boolean value:");
-        boolean g = sc.nextBoolean();
+        boolean g = sc.nextBoolean();//it will read only boolean value
         System.out.println(g);
+
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        System.out.println("age=" + age);
+
+        System.out.println("Enter your height:");
+        int height = sc.nextInt();
+        System.out.println("height=" + height);
+
+        sc.nextLine();
+
+        System.out.println("Enter your name:");
+        String name = sc.next();
+        System.out.println("name=" + name);
+
+        System.out.println("Enter your full name:");
+        String fullname = sc.nextLine();
+        System.out.println("fullname=" + fullname);
     }
 }
+//next() is used to read only one word.It will stop when it find whitespace.
+//nextLine() is used to read full line including whitespace.
---
+++ track/M02/Learn/T4/StudentApplication.java
@@ -0,0 +1,32 @@
+
+class Student {
+
+    int roll;
+    String name;
+    double height;
+
+    void run() {
+        System.out.println("Running");
+    }
+
+    void sleep() {
+        System.out.println("sleeping");
+    }
+}
+
+public class StudentApplication {
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student();
+        s1.roll = 12;
+        s1.name = "kavya";
+        s1.height = 5.1;
+
+        System.out.println(s1.roll);
+        System.out.println(s1.name);
+        System.out.println(s1.height);
+        s1.run();
+        s1.sleep();
+    }
+}
